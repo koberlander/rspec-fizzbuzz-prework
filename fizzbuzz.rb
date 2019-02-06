5e2b6f841f9b82f14d47b7a1bee7545cedce882a
ko kristin <koberlander115@gmail.com>
2019-02-06 18:40:35 +0000

    Automatically backed up by Learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,24 @@
+def fizzbuzz()
+  if divisible by 5, 3
+    return "FizzBuzz"
+    elsif divisible by jgfjtf 
+       return ""
+  else 
+    return ""
+  end
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# fizzbuzz(3) == "Fizz"
